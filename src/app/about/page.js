cd633e1b45927cd24af6bc43a9ea9bb2d1f1d97d
Mateus Pereira <mateus.maia@dbserver.com.br>
2023-11-04 03:05:38 -0300

    ajustes

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -3,32 +3,37 @@ import Card from '@mui/material/Card';
 import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+import Link from 'next/link';
+import YouTubeIcon from '@mui/icons-material/YouTube';
 
 export default function ImgMediaCard() {
     return (
-        <main className='flex flex-col min-h-screen items-center gap-6'>
-            <Card sx={{ maxWidth: 345 }}>
+        <main className='flex container flex-col min-h-screen items-center gap-10'>
+            <Card sx={{ maxWidth: 345,  }}>
                 <CardMedia
                     component="img"
                     alt="Foto do CEO"
                     height="140"
                     title='CEO Mateus Pereira'
-                    image="https://scontent-gru1-2.xx.fbcdn.net/v/t39.30808-6/395420534_6718622404918825_5581690861007147170_n.jpg?_nc_cat=100&ccb=1-7&_nc_sid=5f2048&_nc_ohc=8J7X2LMUkmsAX8UhY_V&_nc_ht=scontent-gru1-2.xx&oh=00_AfBpo_CLaEU6TPaur92NUFnvdImg8P1kS_wYQhtHOnWnYA&oe=6541E96C"
+                    image="https://i.ibb.co/SQdt8D9/Eu.jpg"
                 />
                 <CardContent>
                     <Typography gutterBottom variant="h5" component="div">
                         Mateus Pereira Rodrigues
                     </Typography>
                     <Typography variant="body2" color="text.secondary">
-                        Lizards are a widespread group of squamate reptiles, with over 6,000
-                        species, ranging across all continents except Antarctica
+                        Desenvolverdo Fullstack há mais de 2 anos e meio, apaixonado por tecnologia e conhecimento em geral. Adoro ensinar e passar meus conhecimentos já adquiridos. Veja mais no meu canal! <Link href={"https://www.youtube.com/channel/UCNAiwsbuvi7kQ2eYoXtzPRA"} target='_blank'><strong>Click aqui!</strong></Link> Ou acesse no ícone logo abaixo. 
                     </Typography>
                 </CardContent>
-                <CardActions>
-                    <Button size="small">Share</Button>
-                    <Button size="small">Learn More</Button>
+                <CardActions className='flex  justify-center  '>
+                    <Link href={"https://www.linkedin.com/in/mateuspereira-developer/"} target='_blank'>
+                    <LinkedInIcon fontSize='medium'/>
+                    </Link>
+                    <Link href={"https://www.youtube.com/channel/UCNAiwsbuvi7kQ2eYoXtzPRA"} target='_blank' >
+                        <YouTubeIcon fontSize='medium' />
+                    </Link>
                 </CardActions>
             </Card>
         </main>
--- src/app/footer/page.js
+++ src/app/footer/page.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 function Footer() {
   return (
-    <footer className="bg-gray-200 p-4  bottom-0 w-full">
+    <footer className="bg-gray-400 p-3  bottom-0 w-full">
       <div className="container mx-auto ">
         <p className='text-gray-800 flex justify-center'>Crie seu site agora mesmo!</p>
       </div>
